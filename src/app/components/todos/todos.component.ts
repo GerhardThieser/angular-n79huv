40156a7f79451d74a34a45754d9806f5fc1a28aa
GerhardThieser <gerhard.thieser@gmx.de>
2019-10-29 13:34:43 +0100

    1 file modified

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -3,9 +3,9 @@ import { Todo } from "../../models/Todo";
 import { TodoService } from "../../services/todo.service";
 
 @Component({
-  selector: "app-todos",
-  templateUrl: "./todos.component.html",
-  styleUrls: ["./todos.component.css"]
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
